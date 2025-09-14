b37ad638cd2ac2efc39cc307a5a0eae3dc83eeb5
RomanGolovinn <golovinroman.ev@gmail.com>
2025-09-14 16:24:24 +0300

    change task.md in OPD/lab1 try 2 + change .gitignore and starting programming lab1

---
+++ programming/sem1/labs/lab1/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main{
+    static public void main(String[] args){
+        int[] l = new int[15];
+        for (int i = 0; i < 15; i++){
+            l[i] = i+1;
+        }
+        System.out.println(Arrays.toString(l));
+
+        double[] x = new double[1];
+    }
+}
