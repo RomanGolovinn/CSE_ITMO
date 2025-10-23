a8af283dbdd7bb0e69f7461fd04d8617085bf6e4
RomanGolovinn <golovinroman.ev@gmail.com>
2025-10-23 15:07:43 +0300

    final change scripts lab1 OPD

--- 1-2_OPD/labs/lab1/script4.sh
+++ 1-2_OPD/labs/lab1/script4.sh
@@ -6,7 +6,7 @@ find lab0 -type f -name '*y' -exec grep -n '^' {} + 2> /tmp/errors.txt | sort
 
 find lab0 -type f -name 'g*' -exec grep -n '^' {} + 2> /tmp/errors.txt | sort
 
-find lab0 -type f -name 'v*' -exec grep -n '^' {} + 2> /tmp/errors.txt | sort
+find lab0 -type f -name 'v*' -exec grep -n '^' {} + 2> /tmp/errors.txt | sort -n
 
 cat lab0/delcatty8/vileplume lab0/delcatty8/unfezant lab0/gigalith6/cottonee \
     lab0/gigalith6/venonat lab0/gigalith6/skiploom lab0/gliscor9/arcanine \
