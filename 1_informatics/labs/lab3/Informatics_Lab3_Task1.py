a0bf093ba88cf932711d6ac5a2185c9f942cd4cd
RomanGolovinn <golovinroman.ev@gmail.com>
2025-11-02 23:48:55 +0300

    lab3 inf 1 task

--- 1_informatics/labs/lab3/Informatics_Lab3_Task1.py
+++ 1_informatics/labs/lab3/Informatics_Lab3_Task1.py
@@ -1,17 +1,15 @@
 import re
 
-pattern = r'(?=(\bВТ\b(?:\s+\S+){0,4}?\s+\bИТМО\b))'
-
 def check_text(text : str) -> []:
     fragments = []
-    f1 = re.findall(pattern, text)
-    if f1:
-        for i in f1:
-            fragments.append(i)
+    for k in range(0, 5):
+        pat = rf'(?=(\bВТ\b(?:\s+\S+){{{k}}}\s+\bИТМО\b))'
+        for m in re.finditer(pat, text):
+            fragments.append(m.group(1))
     return fragments
 
 tests = [
-    check_text("BТ а б ИТМО ВТ ИТМО"),
+    check_text("ВТ а б ИТМО ВТ ИТМО"),
     check_text("ВТ ВТ ВТ ИТМО"),
     check_text("Кафедра ВТ появилась в университете ИТМО в 1937"),
     check_text("ВТ ИТМО ВТ ИТМО, я не зна какие ещё тесты придумать с ВТ и ИТМО"),
@@ -19,7 +17,7 @@ tests = [
 ]
 
 results = [
-    ["ВТ а б ИТМО", "ВТ ИТМО"],
+    ["ВТ а б ИТМО", "ВТ ИТМО", "ВТ а б ИТМО ВТ ИТМО"],
     ["ВТ ВТ ВТ ИТМО", "ВТ ВТ ИТМО", "ВТ ИТМО"],
     ["ВТ появилась в университете ИТМО"],
     ["ВТ ИТМО", "ВТ ИТМО ВТ ИТМО", "ВТ ИТМО", "ВТ и ИТМО"],
@@ -28,12 +26,14 @@ results = [
 
 def check_results(tests, results):
     for i in range(5):
-        print(tests[i], results[i])
         isOK = True
         if len(results[i]) != len(tests[i]):
             isOK = False
         for j in range(len(tests[i])):
             if tests[i].count(tests[i][j]) != results[i].count(tests[i][j]):
+                isOK = False
+        for j in range(len(results[i])):
+            if tests[i].count(results[i][j]) != results[i].count(results[i][j]):
                 isOK = False
         if isOK:
             print(f"test{i+1} done")
