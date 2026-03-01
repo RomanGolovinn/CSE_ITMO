e5b269cc8189b2ef6e4aeaf2121d49a7c85e518f
Roman Golovin <golovinroman.ev@gmail.com>
2026-03-01 14:39:50 +0300

    finish execution of lab1 db

--- 2_data_base/labs/lab1/lab1.sql
+++ 2_data_base/labs/lab1/lab1.sql
@@ -5,12 +5,16 @@ CREATE TABLE character(
 CREATE TABLE location(
     location_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    landscape_type VARCHAR(100)
+    landscape_type VARCHAR(100),
+    x DOUBLE PRECISION NOT NULL,
+    y DOUBLE PRECISION NOT NULL,
+    z DOUBLE PRECISION,
+    planet VARCHAR(100)
 );
 CREATE TABLE event(
     event_id SERIAL PRIMARY KEY,
     event_type VARCHAR(100),
-    event_date TIMESTAMP,
+    event_date DATE,
     location_id INTEGER REFERENCES location(location_id)
 );
 CREATE TABLE event_participant(
@@ -26,19 +30,42 @@ CREATE TABLE character_state(
     state_id SERIAL PRIMARY KEY,
     character_id INTEGER REFERENCES character(character_id),
     mindset_id INTEGER REFERENCES mindset(mindset_id),
-    observed_at TIMESTAMP
-);
-CREATE TABLE expectee(
-    character_id INTEGER PRIMARY KEY REFERENCES character(character_id),
-    name VARCHAR(100) NOT NULL
-);
-CREATE TABLE expecter(
-    character_id INTEGER PRIMARY KEY REFERENCES character(character_id),
-    name VARCHAR(100) NOT NULL
+    observed_at INTERVAL
 );
 CREATE TABLE expectation(
     expectation_id SERIAL PRIMARY KEY,
-    observer_id INTEGER REFERENCES expecter(character_id),
-    target_character INTEGER REFERENCES expectee(character_id),
     mindset_id INTEGER REFERENCES mindset(mindset_id)
 );
+CREATE TABLE expectee(
+    expectee_id SERIAL PRIMARY KEY,
+    character_id INTEGER REFERENCES character(character_id),
+    expectation_id INTEGER REFERENCES expectation(expectation_id)
+);
+CREATE TABLE expecter(
+    expecter_id SERIAL PRIMARY KEY,
+    character_id INTEGER REFERENCES character(character_id),
+    expectation_id INTEGER REFERENCES expectation(expectation_id)
+);
+
+
+INSERT INTO character (name) VALUES ('Alvin'), ('Jeserac'), ('Khedron');
+
+INSERT INTO location (name, landscape_type, x, y, z, planet) VALUES ('Castle', 'desert', 300.005, 50, -456.789, 'Earth');
+
+INSERT INTO event (event_type, event_date, location_id) VALUES ('meeting', '220025-01-01', 1);
+
+INSERT INTO mindset (description, is_common) VALUES ('он сильно переменился со времени их последней встречи в башне Лоранна', true);
+
+INSERT INTO expectation (mindset_id) VALUES (1);
+
+
+INSERT INTO event_participant (event_id, character_id) VALUES (1, 1), (1, 2), (1, 3);
+
+INSERT INTO character_state (character_id, mindset_id, observed_at) VALUES
+(1, NULL, NULL), (2, 1, '2 months'), (3, NULL, NULL);
+
+INSERT INTO expectee (character_id, expectation_id) VALUES (2, 1), (3, 1);
+
+INSERT INTO expecter (character_id, expectation_id) VALUES (1, 1);
+
+
