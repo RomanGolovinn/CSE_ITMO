36fbb0f837f6d1dc2cebe5d63b061e2d39fea7fd
Roman Golovin <golovinroman.ev@gmail.com>
2026-03-02 20:59:02 +0300

    wrote abstract class CollectionManager + StackManager (extends CM) lab5 prog

---
+++ 1-2_programming/labs/lab5/ru/ifmo/se/src/managers/collection/CollectionManager.java
@@ -0,0 +1,29 @@
+package managers.collection;
+
+import models.Flat;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+public abstract class CollectionManager {
+    protected LocalDateTime lastInitTime;
+    protected LocalDateTime lastSaveTime;
+
+    public CollectionManager() {
+        this.lastInitTime = LocalDateTime.now();
+    }
+
+    public abstract void add(Flat flat);
+    public abstract boolean removeById(Long id);
+    public abstract void clear();
+    public abstract Flat getById(Long id);
+    public abstract Collection<Flat> getCollection();
+    public abstract void sort();
+
+    // Общий метод для всех (уже реализован)
+    public String getInfo() {
+        return "Тип: " + getCollection().getClass().getSimpleName() +
+                "\nИнициализирован: " + lastInitTime +
+                "\nЭлементов: " + getCollection().size();
+    }
+}
