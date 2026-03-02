f111f45af04c0959983c9eb5f1a6fa6b2c908e54
Roman Golovin <golovinroman.ev@gmail.com>
2026-03-02 20:20:55 +0300

    finish models lab5 prog

--- 1-2_programming/labs/lab5/ru.ifmo.se/src/models/Flat.java
+++ 1-2_programming/labs/lab5/ru.ifmo.se/src/models/Flat.java
@@ -4,7 +4,10 @@ import models.enums.Furnish;
 import models.enums.Transport;
 import models.enums.View;
 
-public class Flat {
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Flat implements Comparable<Flat>{
     private Long id; //Поле не может быть null, Значение поля должно быть больше 0, Значение этого поля должно быть уникальным, Значение этого поля должно генерироваться автоматически
     private String name; //Поле не может быть null, Строка не может быть пустой
     private Coordinates coordinates; //Поле не может быть null
@@ -16,4 +19,48 @@ public class Flat {
     private Transport transport; //Поле может быть null
     private House house; //Поле может быть null
 
+    public Flat(String name, Coordinates coordinates, Long area, Long numberOfRooms,
+                Furnish furnish, View view, Transport transport, House house) {
+        this.name = name;
+        this.coordinates = coordinates;
+        this.creationDate = LocalDateTime.now();
+        this.area = area;
+        this.numberOfRooms = numberOfRooms;
+        this.furnish = furnish;
+        this.view = view;
+        this.transport = transport;
+        this.house = house;
+    }
+
+    public void setID(Long id){
+        this.id = id;
+    }
+    public Long getId(){return id;}
+    public String getName() { return name; }
+
+    @Override
+    public int compareTo(Flat other) {
+        return this.name.compareTo(other.getName());
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Flat flat = (Flat) o;
+        return Objects.equals(id, flat.id) &&
+                Objects.equals(name, flat.name) &&
+                Objects.equals(coordinates, flat.coordinates);
+    }
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (coordinates != null ? coordinates.hashCode() : 0);
+        return result;
+    }
+    @Override
+    public String toString() {
+        return String.format("Flat[ID:%d, Name:%s, Area:%d, Rooms:%d, Furnish:%s]",
+                id, name, area, numberOfRooms, furnish);
+    }
 }
--- 1-2_programming/labs/lab5/ru.ifmo.se/src/models/House.java
+++ 1-2_programming/labs/lab5/ru.ifmo.se/src/models/House.java
@@ -5,4 +5,14 @@ public class House {
     private Integer year; //Поле не может быть null, Значение поля должно быть больше 0
     private long numberOfFloors; //Значение поля должно быть больше 0
 
+    public House(String name, Integer year, long numberOfFloors) {
+        this.name = name;
+        this.year = year;
+        this.numberOfFloors = numberOfFloors;
+    }
+
+    @Override
+    public String toString() {
+        return "House{name='" + name + "', year=" + year + ", floors=" + numberOfFloors + "}";
+    }
 }
