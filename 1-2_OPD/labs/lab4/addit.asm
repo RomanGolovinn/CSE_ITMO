ef4944049ba970f3fcf9a8aaa2a2fd181d4a351a
Roman Golovin <golovinroman.ev@gmail.com>
2026-05-06 22:36:16 +0300

    opd lab4, done addit

--- 1-2_OPD/labs/lab4/addit.asm
+++ 1-2_OPD/labs/lab4/addit.asm
@@ -4,31 +4,30 @@ res: word 0x000
 arrpointer: word 0x100
 counter: word 0x3
 
-start: cla
-iter: ld (arrpointer)+
-call func
-add res
-st res
-loop counter
-jump iter
-ld res
-call func
-st res
-hlt
+start:  cla
+iter:   ld (arrpointer)+
+        push
+        call func
+        pop
+        add res
+        st res
+        loop counter
+        jump iter
+        hlt
 
+func:   ld &1
+        and m_0fff
+        st tmp
+        and m_0800
+        beq is_pos
+        ld tmp
+        add m_f000
+        jump save
+is_pos: ld tmp
+save:   st &1
+        ret
 
-func: 
-and m_0fff
-st tmp
-and m_0800
-beq pos
-ld tmp
-add m_0fff
-ret
-pos: ld tmp
-ret
-
-m_0fff: word 0x0fff
+m_0fff: word 0x0FFF
 m_0800: word 0x0800
-m_f000: word 0x0f00
-tmp: word 0x000
+m_f000: word 0xF000
+tmp:    word 0x0000
