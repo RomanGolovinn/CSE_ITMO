1649a19831e7c84254d4c88099065a643ec5c343
Roman Golovin <golovinroman.ev@gmail.com>
2026-05-12 14:18:46 +0300

    algebra, added some lectures

--- 1-2_OPD/labs/lab4/addit.asm
+++ 1-2_OPD/labs/lab4/addit.asm
@@ -1,5 +1,5 @@
 org 0x100
-word 0x8001, 0x8001, 0x8fff
+word 0x8fff, 0x8001, 0x8ffa
 res: word 0x000
 arrpointer: word 0x100
 counter: word 0x3
@@ -13,6 +13,9 @@ iter:   ld (arrpointer)+
         st res
         loop counter
         jump iter
+        ld res
+        and m_0fff
+        st res
         hlt
 
 func:   ld &1
