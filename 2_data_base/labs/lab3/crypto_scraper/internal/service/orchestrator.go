ed8527bf1ee7140d492e25e93866132bfba10836
Roman Golovin <golovinroman.ev@gmail.com>
2026-05-18 12:25:55 +0300

    db lab3, add sevice layer

--- 2_data_base/labs/lab3/crypto_scraper/internal/service/orchestrator.go
+++ 2_data_base/labs/lab3/crypto_scraper/internal/service/orchestrator.go
@@ -1,1 +1,54 @@
 package service
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"scraper/internal/client"
+	"scraper/internal/storage"
+)
+
+type ScraperService struct {
+	apiClient *client.BinanceClient
+	dbStorage *storage.PostgresStorage
+	symbol    string
+	interval  time.Duration
+}
+
+func NewScraperService(api *client.BinanceClient, db *storage.PostgresStorage, sym string, intv time.Duration) *ScraperService {
+	return &ScraperService{
+		apiClient: api,
+		dbStorage: db,
+		symbol:    sym,
+		interval:  intv,
+	}
+}
+
+func (s *ScraperService) Start() {
+	log.Printf("Запуск скрапера для %s. Интервал: %v\n", s.symbol, s.interval)
+
+	t := time.NewTicker(s.interval)
+	defer t.Stop()
+
+	for {
+		s.processTick()
+		<-t.C
+	}
+}
+
+func (s *ScraperService) processTick() {
+	priceData, err := s.apiClient.FetchPrice(s.symbol)
+	if err != nil {
+		log.Println("Ошибка получения данных от API:", err)
+		return
+	}
+
+	fmt.Printf("[%s] Получена цена: %.2f$\n", priceData.Symbol, priceData.Price)
+
+	err = s.dbStorage.SavePriceDistributed(priceData)
+	if err != nil {
+		log.Println("Ошибка сохранения (2PC):", err)
+		return
+	}
+}
