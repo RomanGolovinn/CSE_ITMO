651e4279b579b5a41d4751a3dee3e0610e74df39
Roman Golovin <golovinroman.ev@gmail.com>
2026-06-08 21:02:34 +0300

    opd lab6, aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- 1-2_OPD/labs/lab6/lab6.asm
+++ 1-2_OPD/labs/lab6/lab6.asm
@@ -1,4 +1,5 @@
-org 0x0
+org 0x00
+
 v0: word $default, 0x180
 v1: word $default, 0x180
 v2: word $vu2, 0x180
@@ -8,74 +9,76 @@ v5: word $default, 0x180
 v6: word $default, 0x180
 v7: word $default, 0x180
 
-default: iret
+default:
+    iret
 
-org 0x21
-x: word 0x0
+org 0x20
+
+x:          word 0x0000
+tmp:        word 0x0000
+
+c_min_33:   word 0xFFDF
+c_max_30:   word 0x001E
+c_31:       word 0x001F
+c_7:        word 0x0007
+c_ff:     word 255
 
 start:
-di
-cla
-ld #0x0a
-out 5
-ld #0x0b
-out 7
-ei
+    di
+
+    ld #0x0A
+    out 5
+
+    ld #0x0B
+    out 7
+
+    ei
 
 main_loop:
-ld x
-dec
-st tmp
-cmp c_max_127
-bpl main_out_odz
-cmp c_min_128
-blt main_out_odz
-ld tmp
-st x
-jump main_loop
+    ld x
+    dec
+    call check_odz
+    jump main_loop
 
-main_out_odz:
-ld c_max_127
-st x
-jump main_loop
+check_odz:
+    st tmp
+
+    cmp c_31
+    bge out_odz
+
+    ld tmp
+    cmp c_min_33
+    blt out_odz
+
+    ld tmp
+    st x
+    ret
+
+out_odz:
+    ld c_max_30
+    st x
+    ret
 
 vu2:
-push
-in 4
-and x
-not
-sxtb 
-st x
-pop
-iret
+    in 4
+    and x
+
+    not
+
+    sxtb
+
+    call check_odz
+    iret
 
 vu3:
-push
-ld x
-asl
-asl
-neg
-sub c_7
-st tmp
-cmp c_max_127
-bpl vu3_out_odz
-ld tmp
-cmp c_min_128
-blt vu3_out_odz
-ld tmp
-jump vu3_save
+    ld x
 
-vu3_out_odz:
-ld c_max_127
+    asl
+    asl
 
-vu3_save:
-st x
-out 6
-pop
-iret
+    neg
 
-tmp: word 0x0
-c_min_128: word 0xff80
-c_max_127: word 0x7f
-c_7: word 0x7
+    sub c_7
 
+    out 6
+    iret
