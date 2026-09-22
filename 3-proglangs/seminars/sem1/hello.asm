22247a4544a61e311fd289fbf803c3ca1e18f9fa
Roman Golovin <golovinroman.ev@gmail.com>
2026-09-22 14:20:56 +0300

    first sem proglang done

---
+++ 3-proglangs/seminars/sem1/hello.asm
@@ -0,0 +1,24 @@
+; hello.asm
+section .data
+message: db  'hello, world!', 10
+err: db 'some error!', 10
+
+section .text
+global _start
+
+_start:
+    mov     rax, 1           ; 'write' syscall number
+    mov     rdi, 1           ; stdout descriptor
+    mov     rsi, message     ; string address
+    mov     rdx, 14          ; string length in bytes
+    syscall
+
+    mov     rax, 1           
+    mov     rdi, 2          
+    mov     rsi, err
+    mov     rdx, 12
+    syscall
+
+    mov     rax, 60          ; 'exit' syscall number
+    xor     rdi, rdi         ; error code
+    syscall
